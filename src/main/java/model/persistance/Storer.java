05e53b59035bcb72182f3a428a6f51f712a9a223
DeOldSax <DeOldSax@gmx.de>
2014-10-25 18:17:53 +0200

    Added encryption.

--- src/main/java/model/persistance/Storer.java
+++ src/main/java/model/persistance/Storer.java
@@ -7,11 +7,28 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.nio.file.Files;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SealedObject;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+import org.apache.log4j.Logger;
 
 public class Storer {
 
 	private String storePath;
+	private Key key;
 
 	public Storer(String storePath) {
 		this.storePath = storePath;
@@ -21,48 +38,7 @@ public class Storer {
 		createStorageFolder();
 		String thisStorePath = createStorePath(storableObject);
 		// TODO check if file already exists and throw exception!
-		serialize(storableObject, thisStorePath);
-	}
-
-	protected Storable load(Storable storableObject) {
-		return (Storable) deserialize(createStorePath(storableObject));
-	}
-
-	private String createStorePath(Storable storableObject) {
-		String thisStorePath = storePath + "/" + storableObject.getStorageFileName();
-		return thisStorePath;
-	}
-
-	private static void serialize(Object object, String path) {
-		ObjectOutputStream out = null;
-		try {
-			FileOutputStream output = new FileOutputStream(path);
-			out = new ObjectOutputStream(output);
-			out.writeObject(object);
-			out.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static Object deserialize(String path) {
-		Object object = null;
-		try {
-			final FileInputStream fileInputStream = new FileInputStream(new File(path));
-			ObjectInputStream o = new ObjectInputStream(fileInputStream);
-			object = o.readObject();
-			o.close();
-		} catch (FileNotFoundException e) {
-			return null;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		} catch (ClassNotFoundException e) {
-			return null;
-		}
-		return object;
+		serializeAndEncrypt(storableObject, thisStorePath);
 	}
 
 	private void createStorageFolder() {
@@ -77,5 +53,116 @@ public class Storer {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	protected Storable load(Storable storableObject) {
+		return (Storable) deserializeAndDecrypt(createStorePath(storableObject));
+	}
+
+	private String createStorePath(Storable storableObject) {
+		String thisStorePath = storePath + "/" + storableObject.getStorageFileName();
+		return thisStorePath;
+	}
+
+	private void serializeAndEncrypt(Serializable object, String path) {
+		SealedObject sealedObject = encryptStorableObject(object);
+		Object storableObject;
+
+		if (sealedObject == null) {
+			storableObject = object;
+		} else {
+			storableObject = sealedObject;
+		}
+		serialize(storableObject, path);
+	}
+
+	private void serialize(Object object, String path) {
+		ObjectOutputStream out = null;
+		try {
+			FileOutputStream output = new FileOutputStream(path);
+			out = new ObjectOutputStream(output);
+			out.writeObject(object);
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Object deserializeAndDecrypt(String path) {
+		Object object = deserialize(path);
+		Storable storable = decryptSealedObject(object);
+		return storable;
+	}
+
+	private Object deserialize(String path) {
+		Object object = null;
+		try {
+			final FileInputStream fileInputStream = new FileInputStream(new File(path));
+			ObjectInputStream o = new ObjectInputStream(fileInputStream);
+			object = o.readObject();
+			o.close();
+		} catch (FileNotFoundException e) {
+			return null;
+		} catch (IOException e) {
+			return null;
+		} catch (ClassNotFoundException e) {
+			return null;
+		}
+		return object;
+	}
+
+	private SealedObject encryptStorableObject(Serializable object) {
+		Cipher cipher = null;
+		KeyGenerator keyGen = null;
+		SealedObject sealedObject = null;
+		try {
+			keyGen = KeyGenerator.getInstance("AES");
+			cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			keyGen.init(128);
+			if (key == null) {
+				key = keyGen.generateKey();
+				serialize(key, storePath + "/" + "key.ser");
+			}
+			byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+			IvParameterSpec ivspec = new IvParameterSpec(iv);
+			cipher.init(Cipher.ENCRYPT_MODE, key, ivspec);
+			sealedObject = new SealedObject(object, cipher);
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
+				| IllegalBlockSizeException | IOException | InvalidAlgorithmParameterException e1) {
+			Logger.getLogger(Storer.class).warn("No Cipher.", e1);
+			e1.printStackTrace();
+		}
+		return sealedObject;
+	}
+
+	private Storable decryptSealedObject(Object object) {
+		if (!(object instanceof SealedObject)) {
+			return null;
+		}
+
+		SealedObject sealedObject = (SealedObject) object;
+		Cipher cipher = null;
+		Storable storable;
+		try {
+			cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			SecretKey key = (SecretKey) deserialize(storePath + "/" + "key.ser");
+
+			byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+			IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+			cipher.init(Cipher.DECRYPT_MODE, key, ivspec);
+
+			storable = (Storable) sealedObject.getObject(cipher);
+
+		} catch (ClassCastException | NoSuchAlgorithmException | NoSuchPaddingException
+				| InvalidKeyException | IllegalBlockSizeException | IOException
+				| ClassNotFoundException | BadPaddingException | InvalidAlgorithmParameterException e1) {
+			Logger.getLogger(Storer.class).warn("No Cipher.", e1);
+			e1.printStackTrace();
+			return null;
+		}
+		return storable;
 	}
 }
