9c5e0d2f727f8b9e66d9b391b9fb3d5bb65a24fc
David Englert <EnglertD@gmx.de>
2020-12-06 15:36:32 +0100

    upgrade junit dependency to 4.13

--- src/test/java/utils/IliasCourseFormatterTest.java
+++ src/test/java/utils/IliasCourseFormatterTest.java
@@ -1,29 +1,29 @@
 package utils;
 
-import junit.framework.Assert;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class IliasCourseFormatterTest {
 
-	@Test
-	public void testFormatting() {
-		String courseName = "[938493] TestCourse eins 5 [info7]";
-		String formattedCourseName = IliasCourseFormatter.formatCourseName(courseName);
-		Assert.assertEquals("TestCourse eins 5 [info7]", formattedCourseName);
-	}
+    @Test
+    public void testFormatting() {
+        String courseName = "[938493] TestCourse eins 5 [info7]";
+        String formattedCourseName = IliasCourseFormatter.formatCourseName(courseName);
+        assertEquals("TestCourse eins 5 [info7]", formattedCourseName);
+    }
 
-	@Test
-	public void testFormatting1() {
-		String courseName = " [938493] TestCourse";
-		String formattedCourseName = IliasCourseFormatter.formatCourseName(courseName);
-		Assert.assertEquals("TestCourse", formattedCourseName);
-	}
+    @Test
+    public void testFormatting1() {
+        String courseName = " [938493] TestCourse";
+        String formattedCourseName = IliasCourseFormatter.formatCourseName(courseName);
+        assertEquals("TestCourse", formattedCourseName);
+    }
 
-	@Test
-	public void testFormatting2() {
-		String courseName = "TestCourse [infotext5]";
-		String formattedCourseName = IliasCourseFormatter.formatCourseName(courseName);
-		Assert.assertEquals(courseName, formattedCourseName);
-	}
+    @Test
+    public void testFormatting2() {
+        String courseName = "TestCourse [infotext5]";
+        String formattedCourseName = IliasCourseFormatter.formatCourseName(courseName);
+        assertEquals(courseName, formattedCourseName);
+    }
 }
